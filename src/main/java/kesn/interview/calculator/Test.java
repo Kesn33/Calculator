e0e3b68a0937369dda7795a12dd9a521ec1e8213
kesn33 <1874107094@qq.com>
2023-05-10 16:22:49 +0800

    新增测试用例

--- src/main/java/kesn/interview/calculator/Test.java
+++ src/main/java/kesn/interview/calculator/Test.java
@@ -8,30 +8,35 @@ public class Test {
 
     public static void main(String[] args) {
         CalculatorImpl calculator = new CalculatorImpl();
-        calculator.redo();
+        //正常执行
         calculator.add(BigDecimal.valueOf(1));
-        calculator.redo();
-        calculator.redo();
-        calculator.redo();
         calculator.sub(BigDecimal.valueOf(2));
         calculator.mul(BigDecimal.valueOf(3));
         calculator.div(BigDecimal.valueOf(4));
-        calculator.undo();
+        calculator.getResult();
+
+        //模拟redo，undo执行
+        calculator.redo();
+        calculator.getResult();
         calculator.add(BigDecimal.valueOf(5));
         calculator.sub(BigDecimal.valueOf(6));
         calculator.mul(BigDecimal.valueOf(7));
         calculator.div(BigDecimal.valueOf(8));
         calculator.div(BigDecimal.valueOf(100));
-        calculator.getResult();
         calculator.undo();
         calculator.getResult();
         calculator.undo();
-        calculator.undo();
-        calculator.undo();
-        calculator.undo();
-        calculator.undo();
-        calculator.undo();
-        calculator.undo();
+        calculator.getResult();
+
+        //模拟除数是0
+//        calculator.div(BigDecimal.ZERO);
+
+        //模拟一直undo
+        /*for (int i = 0; i < 10; i ++){
+            calculator.undo();
+        }*/
+
+        //模拟多线程下得原子性
         calculator.redo();
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         for (int i = 0; i< 30; i ++){
