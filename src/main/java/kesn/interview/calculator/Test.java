938672386aea4ddaf627cee2eda91d4579fc0ddd
kesn33 <1874107094@qq.com>
2023-05-10 16:28:24 +0800

    新增测试用例

--- src/main/java/kesn/interview/calculator/Test.java
+++ src/main/java/kesn/interview/calculator/Test.java
@@ -9,6 +9,7 @@ public class Test {
     public static void main(String[] args) {
         CalculatorImpl calculator = new CalculatorImpl();
         //正常执行
+        System.out.println("正常执行----------------------");
         calculator.add(BigDecimal.valueOf(1));
         calculator.sub(BigDecimal.valueOf(2));
         calculator.mul(BigDecimal.valueOf(3));
@@ -16,6 +17,7 @@ public class Test {
         calculator.getResult();
 
         //模拟redo，undo执行
+        System.out.println("模拟redo，undo执行----------------------");
         calculator.redo();
         calculator.getResult();
         calculator.add(BigDecimal.valueOf(5));
@@ -29,14 +31,17 @@ public class Test {
         calculator.getResult();
 
         //模拟除数是0
-//        calculator.div(BigDecimal.ZERO);
+        /*System.out.println("模拟除数是0----------------------");
+        calculator.div(BigDecimal.ZERO);*/
 
         //模拟一直undo
-        /*for (int i = 0; i < 10; i ++){
+        /*System.out.println("模拟一直undo----------------------");
+        for (int i = 0; i < 10; i ++){
             calculator.undo();
         }*/
 
-        //模拟多线程下得原子性
+        //模拟多线程下的原子性
+        System.out.println("模拟多线程下的原子性----------------------");
         calculator.redo();
         ExecutorService executorService = Executors.newFixedThreadPool(5);
         for (int i = 0; i< 30; i ++){
